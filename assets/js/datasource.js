f3eefc96140051e468bb038fe825b09c064e4a7f
thangnm11 <thang.991992@gmail.com>
2020-05-19 10:25:38 +0700

    FE deploy to apps.nguyenmanhthang.net

---
+++ assets/js/datasource.js
@@ -0,0 +1,90 @@
+var data = [
+	{
+		"panel_title": "NguyenManhThang.Net",
+		"aria_expanded": "true",
+		"panel_class": "panel-success",
+		"panel_body": [
+			{
+				"icon": "fa-vcard",
+				"text": "Profile<br>(ThangNM)",
+				"url_webapp": "https://profile.nguyenmanhthang.net",
+				"panel_class": "panel-info"
+			},
+			{
+				"icon": "fa-connectdevelop",
+				"text": "Curriculum Vitae<br>(CV)",
+				"url_webapp": "https://cv.nguyenmanhthang.net",
+				"panel_class": "panel-success"
+			},
+			{
+				"icon": "fa-heartbeat",
+				"text": "Wedding<br>(NMT & NTHD)",
+				"url_webapp": "https://wedding.nguyenmanhthang.net",
+				"panel_class": "panel-info"
+			}
+		]
+	},
+	{
+		"panel_title": "Pet Project",
+		"aria_expanded": "true",
+		"panel_class": "panel-info",
+		"panel_body": [
+			{
+				"icon": "fa-file-code-o",
+				"text": "Code Generation Tool<br>.",
+				"url_webapp": "https://code.nguyenmanhthang.net/",
+				"panel_class": "panel-warning"
+			},
+			{
+				"icon": "fa-sitemap",
+				"text": "Genealogy",
+				"url_webapp": "https://genealogy.nguyenmanhthang.net/",
+				"panel_class": "panel-danger"
+			}
+		]
+	},
+	{
+		"panel_title": "Open-source software (OSS)",
+		"aria_expanded": "true",
+		"panel_class": "panel-warning",
+		"panel_body": [
+			{
+				"icon": "fa-cubes",
+				"text": "<a href='https://www.mrcms.com/' target='_blank'>Mr CMS</a><br>(<a href='https://github.com/MrCMS/MrCMS' target='_blank'>ASP.NET MVC C#</a>)",
+				"url_webapp": "https://cms.nguyenmanhthang.net",
+				"panel_class": "panel-info"
+			},
+			{
+				"icon": "fa-map",
+				"text": "<a href='https://www.openstreetmap.org/' target='_blank'>OSM</a> - <a href='https://leafletjs.com/' target='_blank'>Leaflet</a><br>(<a href='https://github.com/Leaflet/Leaflet' target='_blank'>JavaScript</a>)",
+				"url_webapp": "https://map.nguyenmanhthang.net",
+				"panel_class": "panel-info"
+			},
+			{
+				"icon": "fa-key",
+				"text": "<a href='https://keeweb.info/' target='_blank'>KeeWeb</a> - <a href='https://keepass.info/' target='_blank'>KeePass</a><br>(<a href='https://github.com/keeweb/keeweb' target='_blank'>JavaScript</a>)",
+				"url_webapp": "https://keeweb.nguyenmanhthang.net",
+				"panel_class": "panel-info"
+			},
+			{
+				"icon": "fa-dashboard",
+				"text": "<a href='https://kanboard.org/' target='_blank'>Kanboard</a> - <a href='https://www.atlassian.com/agile/kanban' target='_blank'>Kanban</a><br>(<a href='https://github.com/kanboard/kanboard' target='_blank'>PHP</a>)",
+				"url_webapp": "https://kanban.nguyenmanhthang.net",
+				"panel_class": "panel-info"
+			}
+		]
+	},
+	{
+		"panel_title": "Research & Development (R&D)",
+		"aria_expanded": "true",
+		"panel_class": "panel-danger",
+		"panel_body": [
+			{
+				"icon": "fa-bitcoin",
+				"text": "<a href='https://github.com/lhartikk/naivechain' target='_blank'>Blockchain</a> - <a href='https://github.com/lhartikk/naivecoin' target='_blank'>Coin</a> - <a href='https://github.com/lhartikk/naivecoin-ui' target='_blank'>Wallet</a>",
+				"url_webapp": "https://nmt-wallet.thangnm.tk",
+				"panel_class": "panel-info"
+			}
+		]
+	}
+]
